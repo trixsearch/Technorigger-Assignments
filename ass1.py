925ca327cd095ce8fffea1fb4396cac4e5c72bd7
Abhinav Sahu <70811457+trixsearch@users.noreply.github.com>
2022-01-28 23:37:01 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,7 @@
+"""Python Script to Add Two No."""
+a=float(input("Enter first No. : "))
+b=float(input("Enter Second No. : "))
+sum=a+b
+print("The addition of your numbers is : ", sum)
+print('this program is made by @trixsearch for Technorigger')
+"""Here Script is end"""
