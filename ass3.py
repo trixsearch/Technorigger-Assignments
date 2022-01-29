147955b2bbd97ce90302aebd96002c8fdfd3ed03
Abhinav Sahu <70811457+trixsearch@users.noreply.github.com>
2022-01-29 21:55:14 +0530

    Add files via upload

---
+++ ass3.py
@@ -0,0 +1,10 @@
+#This is my Third Assignment
+#Assignment 3 : To Find Simple Interest with user value
+print("Assignment 3 : To find simple interest")
+print("\n Please provide us the required values")
+p=float(input("write principal balance : "))
+r=float(input("Write the rate of interest without % mark : "))
+t=float(input("Write the given time : "))
+si=p*r*t
+print("The required sinple interest will be : ",si)
+""""This program is writter by @trixsearch for technorigger"""
---
+++ ass4.py
@@ -0,0 +1,9 @@
+#This is Assignment 4 of technorigger
+#This assignment is made by @trixsearch
+print("Assignment 4 : To find the Volume of the cuboid")
+print("\n Please provide us the required length")
+l=float(input("length = "))
+b=float(input("breadth = "))
+h=float(input("height = "))
+v=l*b*h
+print("Your Volume of the cuboid is : ",v)
