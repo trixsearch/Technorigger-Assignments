d42a3d87c0d00dced6b5186762a6c46a51f9faef
Abhinav Sahu <70811457+trixsearch@users.noreply.github.com>
2022-01-30 12:27:48 +0530

    Add files via upload

---
+++ ass8.py
@@ -0,0 +1,14 @@
+#Assignment No. 8 : Python Script to check greatest aoung three no.s
+#This program uses the
+print("Assignment 8 : To find greatest amoung three no.")
+first=float(input("Write your first no. : "))
+second=float(input("Write your second no. : "))
+third=float(input("Write your third no. : "))
+if ((first>second) and (first>third)):
+        print("First one iss greatest")
+elif ((second>first) and (second>third)):
+        print("Second is greatest")
+
+else:
+    print("Third is Greatest")
+print("\n this program is made by trixsearch for Technorigger")
---
+++ ass9.py
@@ -0,0 +1,7 @@
+#Assignment 9: To check whether the given year is leap year or not usning python
+print("Assignment 9 : To check whether the given year is leap year or not")
+year=int(input("Write your Year : "))
+if (year%4)==0 :
+    print("This year is Leap year")
+else:
+    print("The Given year is not a leap year")
